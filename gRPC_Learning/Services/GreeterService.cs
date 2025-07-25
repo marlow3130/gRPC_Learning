62182386a3cf9a9dec9d3558b9a8a338945e0d8b
marlow3130 <Luca.Stalder@wiss-edu.ch>
2025-07-25 09:16:47 +0200

    first commit

---
+++ gRPC_Learning/Services/GreeterService.cs
@@ -0,0 +1,28 @@
+using Grpc.Core;
+using gRPC_Learning;
+
+namespace gRPC_Learning.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+    public override Task<HelloReply> SayGoodbye(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Goodbye " + request.Name
+        });
+    }
+}
